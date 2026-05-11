898be870a63dfffe0c5d41545607d87a94b499d4
JasonMisco11 <jasonmisco11@gmail.com>
2026-05-11 16:58:28 +0000

    mobile view admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,7 +10,7 @@ const AdminPage = async () => {
   const appointments = await getRecentAppointmentList();
 
   return (
-    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+    <div className="mx-auto flex max-w-7xl flex-col space-y-8 sm:space-y-14">
       <header className="admin-header">
         <Link href="/" className="cursor-pointer">
           <Image
